02aa4cbd89ef08a7413a620dd40b069ba7a8acb5
juniuszhangserai <86402568+juniuszhangserai@users.noreply.github.com>
2021-11-05 13:40:54 +0800

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+async function run(): Promise<void> {
+  try {
+    const token = (core.getInput('github_token') ||
+      process.env.GITHUB_TOKEN) as string
+
+    const sha = core.getInput('github_sha')
+
+    const octokit = new github.GitHub(token)
+    const context = github.context
+
+    const environment = core.getInput('environment')
+
+    const request = await octokit.repos.listDeployments({
+      ...context.repo,
+      environment,
+      sha:sha
+    })
+
+    const deployments = request.data
+
+    if (deployments.length > 0) {
+      core.setOutput('deployment_id', deployments[0].id.toString())
+    }
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
